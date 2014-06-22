411359fdcbb53f8a4c5e488869fb7d71b6732bd6
Shane Conder <kf6nvr@gmail.com>
2014-06-22 16:00:51 -0400

    Basic information

--- php/index.php
+++ php/index.php
@@ -1,8 +1,12 @@
 <html>
   <head>
-     <title>Hello World</title>
+     <title>Action Watcher</title>
   </head>
   <body>
-     <?php echo '<p>Hello World</p>'; ?>
+     <?php echo '<p>Action Watcher</p>'; ?><br />
+
+  <hr />
+
+  A simple App Engine app to add some state to some IFTTT rules. Receives messages from IFTTT (via email). Monitors for not receiving messages and, if not received, notifies a different IFTTT trigger.
   </body>
 </html>
